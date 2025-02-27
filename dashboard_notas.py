6e9718ef30d8b89815a254f65b0e2a888335482f
Fernando Castillo <fcastilho999@gmail.com>
2025-02-27 13:50:21 -0300

    working2

--- dashboard_notas.py
+++ dashboard_notas.py
@@ -1,23 +1,128 @@
+# ---------------------------------------------------------------------------- #
+#                                  BIBLIOTECAS                                 #
+# ---------------------------------------------------------------------------- #
 import streamlit as st
 import pandas as pd
 
+# ---------------------------------------------------------------------------- #
+#                           CARREGAR E CONFIGURAR CSV                          #
+# ---------------------------------------------------------------------------- #
 # Carregar o CSV diretamente do GitHub
 url = "https://raw.githubusercontent.com/fernandocastillovicencio/medias-UB-castillo/main/modelo_notas.csv"
 df = pd.read_csv(url)
 
-# Interface do Streamlit
+# Converter a coluna RA para string
+df['RA'] = df['RA'].astype(str)
+
+# ---------------------------------------------------------------------------- #
+#                             STREAM LIST INTERFACE                            #
+# ---------------------------------------------------------------------------- #
+# Título do aplicativo
 st.title("Consulta de Notas")
 
-# Criação da lista suspensa de RAs
-rAs = df['RA'].astype(str).tolist()  # Converte os RAs para string para exibir na lista
-ra_selecionado = st.selectbox("Escolha seu RA:", rAs)
+# ---------------------------------------------------------------------------- #
+#                                  CRIAR LISTA                                 #
+# ---------------------------------------------------------------------------- #
+# Ordenar a lista de RAs em ordem crescente
+ra = ['---'] + sorted(df['RA'].astype(int).tolist())  # Converte para inteiro, ordena e depois volta para string
 
-if ra_selecionado:
-    # Filtra os dados do aluno com base no RA selecionado
-    aluno = df[df['RA'] == int(ra_selecionado)]
 
+# Exibir a lista suspensa e definir "---" como a opção padrão
+ra_selecionado = st.selectbox("Escolha seu RA:", ra, index=0)
+
+# ---------------------------------------------------------------------------- #
+#                                 MOSTRAR NOTAS                                #
+# ---------------------------------------------------------------------------- #
+# Exibir as notas apenas se um RA válido for selecionado
+# Exibir as notas apenas se um RA válido for selecionado
+if str(ra_selecionado) != '---':  # Garante que o valor seja comparado como string
+    aluno = df[df['RA'] == str(ra_selecionado)]  # Filtra os dados do aluno com base no RA selecionado
+
+# ---------------------------------------------------------------------------- #
     if not aluno.empty:
-        st.write("Notas do aluno:")
-        st.write(aluno)
-    else:
-        st.write("RA não encontrado.")
+        
+        st.markdown("---")
+
+        # ----------------{--------------- NOME ------------------------------- #
+        nome = aluno['NC'].values[0]
+        matricula = aluno['RA'].values[0]
+
+        st.write(f"## ALUNO: {nome} -- RA: {matricula}")
+        
+        st.markdown("---")
+
+        # -------------------------------------------------------------------- #
+        #                               MÓDULO 1                               #
+        # -------------------------------------------------------------------- #
+        st.markdown(f"### MÓDULO 1:")
+
+        # ------------------------ atividades em sala ------------------------ #
+        st.markdown(f"##### 1. Atividades em sala (35%):")
+
+        # ----------------------- listas de exercícios ----------------------- #
+        st.markdown(f"##### 2. Lista de Exercícios (10%):")
+
+        # ------------------------------ artigo ------------------------------ #
+        st.markdown(f"##### 3. Artigo da Disciplina (20%):")
+
+        # --------------------------- prova escrita -------------------------- #
+        st.markdown(f"##### 4. Prova Escrita (35%):")
+
+        st.markdown("---")
+
+        # -------------------------------------------------------------------- #
+        #                               MÓDULO 2                               #
+        # -------------------------------------------------------------------- #
+        st.markdown(f"### MÓDULO 2:")
+
+        # ------------------------ atividades em sala ------------------------ #
+        st.markdown(f"##### 1. Atividades em sala (35%):")
+
+        # ----------------------- listas de exercícios ----------------------- #
+        st.markdown(f"##### 2. Lista de Exercícios (10%):")
+
+        # ------------------------------ artigo ------------------------------ #
+        st.markdown(f"##### 3. Artigo da Disciplina (20%):")
+
+        # --------------------------- prova escrita -------------------------- #
+        st.markdown(f"##### 4. Prova Escrita (35%):")
+
+        st.markdown("---")
+
+        # -------------------------------------------------------------------- #
+        #                               MÓDULO 3                               #
+        # -------------------------------------------------------------------- #
+        st.markdown(f"### MÓDULO 3:")
+
+        # ------------------------ atividades em sala ------------------------ #
+        st.markdown(f"##### 1. Atividades em sala (35%):")
+
+        # ----------------------- listas de exercícios ----------------------- #
+        st.markdown(f"##### 2. Lista de Exercícios (10%):")
+
+        # ------------------------------ artigo ------------------------------ #
+        st.markdown(f"##### 3. Artigo da Disciplina (20%):")
+
+        # --------------------------- prova escrita -------------------------- #
+        st.markdown(f"##### 4. Prova Escrita (35%):")
+
+        st.markdown("---")
+
+        # -------------------------------------------------------------------- #
+        #                               MÓDULO 3                               #
+        # -------------------------------------------------------------------- #
+        st.markdown(f"### MÓDULO 4:")
+
+        # ------------------------ atividades em sala ------------------------ #
+        st.markdown(f"##### 1. Atividades em sala (35%):")
+
+        # ----------------------- listas de exercícios ----------------------- #
+        st.markdown(f"##### 2. Lista de Exercícios (10%):")
+
+        # ------------------------------ artigo ------------------------------ #
+        st.markdown(f"##### 3. Artigo da Disciplina (20%):")
+
+        # --------------------------- prova escrita -------------------------- #
+        st.markdown(f"##### 4. Prova Escrita (35%):")
+
+        st.markdown("---")
