feb0ae3744927205ff994449ce5d465b969c10d0
Fernando Castillo <fcastilho999@gmail.com>
2025-03-19 23:47:19 -0300

    a

--- functions.py
+++ functions.py
@@ -27,10 +27,10 @@ def carregar_dados(disciplina):
         # )
         return pd.read_csv("fen1.csv")
     elif disciplina == "Hidráulica e Pneumática":
-        # return pd.read_csv(
-        #     "https://raw.githubusercontent.com/fernandocastillovicencio/medias-UB-castillo/main/hid2.csv"
-        # )
-        return pd.read_csv("hid2za.csv")
+        return pd.read_csv(
+            "https://raw.githubusercontent.com/fernandocastillovicencio/medias-UB-castillo/main/hid2tttt.csv"
+        )
+        # return pd.read_csv("hid2za.csv")
     return None
 
 
