25d73e4a9d1fd05a1bbb5d29c206b7f9b454106a
Fernando Castillo <fcastilho999@gmail.com>
2025-04-23 16:34:05 -0300

    update db-HiP-1a.csv, db-RAC-1a.csv and functions.py

--- functions.py
+++ functions.py
@@ -7,21 +7,21 @@ import streamlit as st
 
 
 # @st.cache_data
-def carregar_dados(disciplina):
-    """
-    Função para carregar os dados de acordo com a disciplina selecionada
-    """
-    if disciplina == "Refrigeração e Ar-Condicionado":
-        # return pd.read_csv(
-        #     "https://raw.githubusercontent.com/fernandocastillovicencio/medias-UB-castillo/main/ref2.csv"
-        # )
-        return pd.read_csv("db-RAC-1a.csv")
-    elif disciplina == "Máquinas de Fluxo":
-        return pd.read_csv("db-MaF-1a.csv")
-    elif disciplina == "Fenômenos de Transporte":
-        return pd.read_csv("db-FdT-1a.csv")
-    elif disciplina == "Hidráulica e Pneumática":
-        return pd.read_csv("db-HiP-1a.csv")
+# def carregar_dados(disciplina):
+#     """
+#     Função para carregar os dados de acordo com a disciplina selecionada
+#     """
+#     if disciplina == "Refrigeração e Ar-Condicionado":
+#         # return pd.read_csv(
+#         #     "https://raw.githubusercontent.com/fernandocastillovicencio/medias-UB-castillo/main/ref2.csv"
+#         # )
+#         return pd.read_csv("db-RAC-1a.csv")
+#     elif disciplina == "Máquinas de Fluxo":
+#         return pd.read_csv("db-MaF-1a.csv")
+#     elif disciplina == "Fenômenos de Transporte":
+#         return pd.read_csv("db-FdT-1a.csv")
+#     elif disciplina == "Hidráulica e Pneumática":
+#         return pd.read_csv("db-HiP-1a.csv")
 
 
 #     return None
