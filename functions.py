a3d8d9c3dc43ab5e4a6b0c8426e614bd092444e5
Fernando Castillo <fcastilho999@gmail.com>
2025-05-27 17:34:04 -0300

    update functions.py

--- functions.py
+++ functions.py
@@ -16,15 +16,15 @@ def carregar_dados(disciplina):
     """
     if disciplina == "Refrigeração e Ar-Condicionado":
         # return pd.read_csv(
-    #     #     "https://raw.githubusercontent.com/fernandocastillovicencio/medias-UB-castillo/main/ref2.csv"
-    #     # )
-    #     return pd.read_csv("db-RAC-2b.csv")
-    # elif disciplina == "Máquinas de Fluxo":
-    #     return pd.read_csv("db-MaF-2a.csv")
-    # elif disciplina == "Fenômenos de Transporte":
-    #     return pd.read_csv("db-FdT-2c.csv")
-    # elif disciplina == "Hidráulica e Pneumática":
-    #     return pd.read_csv("db-HiP-2b.csv")
+        #     #     "https://raw.githubusercontent.com/fernandocastillovicencio/medias-UB-castillo/main/ref2.csv"
+        #     # )
+        return pd.read_csv("db-RAC-2b.csv")
+    elif disciplina == "Máquinas de Fluxo":
+        return pd.read_csv("db-MaF-2a.csv")
+    elif disciplina == "Fenômenos de Transporte":
+        return pd.read_csv("db-FdT-2c.csv")
+    elif disciplina == "Hidráulica e Pneumática":
+        return pd.read_csv("db-HiP-2b.csv")
 
 
 # ---------------------------------------------------------------------------- #
