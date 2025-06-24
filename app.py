eaa00256d6e6d92eb90f8ab88f6c8b1144979ad1
Fernando Castillo <fcastilho999@gmail.com>
2025-06-24 07:20:14 -0300

    Primeiro commit do projeto de médias 2025

---
+++ app.py
@@ -0,0 +1,88 @@
+# # ---------------------------------------------------------------------------- #
+# #                                  BIBLIOTECAS                                 #
+# # ---------------------------------------------------------------------------- #
+# import pandas as pd
+# import streamlit as st
+
+# from functions import carregar_dados, mostrar_modulo
+
+# # Título do aplicativo
+# st.image("logoub.png", width=300)
+# st.markdown("#### Escola Politécnica")
+# st.markdown("##### Prof. Fernando Castillo Vicencio")
+# st.markdown("##### Médias 2025/1")
+
+# # ---------------------------------------------------------------------------- #
+# #                                CARREGAR TURMA                                #
+# # ---------------------------------------------------------------------------- #
+# # Defina as opções de disciplinas com a chave sendo o código e o valor o nome da disciplina
+# disciplinas = {
+#     "RAA": "Refrigeração e Ar-Condicionado",
+#     "MaF": "Máquinas de Fluxo",
+#     "FdT": "Fenômenos de Transporte",
+#     "HiP": "Hidráulica e Pneumática",
+# }
+
+# # Exibe o selectbox para o usuário escolher a disciplina
+# disciplina_selecionada = st.selectbox(
+#     "Escolha a Disciplina:", ["---"] + sorted(disciplinas.values())
+# )
+
+# # Inicializa o dataframe
+# df = None
+
+# # Reseta o RA toda vez que a disciplina mudar
+# if (
+#     "ra_selecionado" in st.session_state
+#     and disciplina_selecionada != st.session_state.disciplina_selecionada_anterior
+# ):
+#     st.session_state.ra_selecionado = "---"  # Reseta o RA
+
+# # Salva a disciplina selecionada
+# st.session_state.disciplina_selecionada_anterior = disciplina_selecionada
+
+# # Exibe o nome da disciplina antes do título "Consulta de Notas"
+# if disciplina_selecionada != "---":
+#     st.write(f"## {disciplina_selecionada}")
+
+# # Carrega o arquivo CSV de acordo com a disciplina selecionada usando a função de cache
+# if disciplina_selecionada != "---":
+#     df = carregar_dados(disciplina_selecionada)
+
+# # ---------------------------------------------------------------------------- #
+# #                                    MOSTRAR                                   #
+# # ---------------------------------------------------------------------------- #
+
+# # Verifique se o dataframe foi carregado corretamente
+# if df is not None:
+#     # Ordenar a lista de RAs em ordem crescente
+#     df["RA"] = df["RA"].astype(str)
+#     ra = ["---"] + sorted(
+#         df["RA"].astype(int).tolist()
+#     )  # Converte para inteiro, ordena e depois volta para string
+
+#     # Exibir a lista suspensa e definir "---" como a opção padrão
+#     ra_selecionado = st.selectbox("Escolha seu RA:", ra, index=0)
+
+#     # Salvar a seleção de RA no session_state (sem precisar verificar se já existe ou não)
+#     st.session_state.ra_selecionado = ra_selecionado
+
+#     # Exibir as notas apenas se um RA válido for selecionado
+#     if str(ra_selecionado) != "---":  # Garante que o valor seja comparado como string
+#         aluno = df[
+#             df["RA"] == str(ra_selecionado)
+#         ]  # Filtra os dados do aluno com base no RA selecionado
+
+#         if not aluno.empty:
+#             # ----------------{--------------- NOME ------------------------------- #
+#             nome = aluno["NC"].values[0]
+#             matricula = aluno["RA"].values[0]
+
+#             st.write(f"#### Aluno(a): {nome}")
+#             st.write(f"#### RA: {matricula}")
+
+#             st.markdown("---")
+
+#             # Exibindo os módulos de forma dinâmica (loop para módulos 1 a 4)
+#             for i in range(1, 5):
+#                 mostrar_modulo(i, aluno)
---
+++ functions.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import streamlit as st
+
+# ---------------------------------------------------------------------------- #
+#                           CALCULAR E MOSTRAR NOTAS                           #
+# ---------------------------------------------------------------------------- #
+
+
+# @st.cache_data
+# ---------------------------------------------------------------------------- #
+
+
+# def carregar_dados(disciplina):
+#     """
+#     Função para carregar os dados de acordo com a disciplina selecionada
+#     """
+#     if disciplina == "Refrigeração e Ar-Condicionado":
+#         #     # return pd.read_csv(
+#         #     #     #     "https://raw.githubusercontent.com/fernandocastillovicencio/medias-UB-castillo/main/ref2.csv"
+#         #     # )
+#         return pd.read_csv("db-RAC-2d.csv")
+#     elif disciplina == "Máquinas de Fluxo":
+#         return pd.read_csv("db-MaF-2d.csv")
+#     elif disciplina == "Fenômenos de Transporte":
+#         return pd.read_csv("db-FdT-2e.csv")
+#     elif disciplina == "Hidráulica e Pneumática":
+#         return pd.read_csv("db-HiP-2a.csv")
+
+
+# ---------------------------------------------------------------------------- #
+
+
+def calcular_media(colunas):
+    """
+    Função para calcular a média, ignorando NaN e valores ausentes
+    """
+    media = (
+        colunas.apply(pd.to_numeric, errors="coerce").mean(axis=1, skipna=True).mean()
+    )
+    return round(media, 2) if pd.notna(media) else 0.0
+
+
+def calcular_e_mostrar_notas(aluno, modulo, prefixo):
+    """
+    Função para calcular e mostrar as notas do aluno
+    """
+    categoria = f"{modulo}-{prefixo}"
+    colunas = aluno.filter(regex=categoria)
+
+    # ---------------------------- formatar tabela --------------------------- #
+    colunas = colunas.applymap(
+        lambda x: f"{x:.2f}".rstrip("0").rstrip(".") if pd.notna(x) else x
+    )
+
+    # --------------------------------- peso --------------------------------- #
+    peso_dict = {"S": 3.5, "Q": 3.5, "L": 1.0, "A": 2.0}
+    peso = peso_dict.get(prefixo, 0.0)
+
+    # ----------------------------- média e nota ----------------------------- #
+    nota = 0
+    if colunas.notna().any().any():
+        # média
+        media = calcular_media(colunas)
+        # nota
+        if pd.notna(media):
+            nota = media * peso
+            # mostrar tabela
+            st.table(colunas)
+            # mostrar média
+            st.markdown(
+                f"\tMédia: **{media:.2f}** (máx. 1.0) -- Pontos: **{nota:.2f}** (máx. {peso})"
+            )
+
+    return nota
+
+
+# ---------------------------------------------------------------------------- #
+#                                MOSTRAR MÓDULO                                #
+# ---------------------------------------------------------------------------- #
+def mostrar_modulo(index, aluno):
+    """
+    Função para exibir as notas do módulo, passando as respectivas informações
+    """
+    st.markdown(f"### MÓDULO {str(index)}:")
+    # -------------------------- atividades em sala -------------------------- #
+    st.markdown(f"##### 1. Atividades em sala (35%):")
+    nota_sala = calcular_e_mostrar_notas(aluno, index, "S")
+    # ------------------------- listas de exercícios ------------------------- #
+    st.markdown(f"##### 2. Lista de Exercícios (10%):")
+    nota_listas = calcular_e_mostrar_notas(aluno, index, "L")
+    # -------------------------------- artigo -------------------------------- #
+    st.markdown(f"##### 3. Artigo da Disciplina (20%):")
+    nota_artigo = calcular_e_mostrar_notas(aluno, index, "A")
+    # ----------------------------- prova escrita ---------------------------- #
+    st.markdown(f"##### 4. Prova Escrita (35%):")
+    nota_prova = calcular_e_mostrar_notas(aluno, index, "Q")
+    # ------------------------------ média total ----------------------------- #
+    media_modulo = nota_sala + nota_listas + nota_artigo + nota_prova
+    # media_modulo = round(media_modulo, 2)
+    if media_modulo > 0.0:
+        st.markdown(f"##### Média do Módulo (Total de Pontos): {media_modulo:.2f}")
+    st.markdown("---")
